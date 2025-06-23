837a1467853b1083bb4ff462c0bd5bf31bbd9220
Albert Waneroba <waneroba22@gmail.com>
2025-06-23 04:10:24 +0300

    modified and installed whitenoise n static

--- FarmerApi/settings.py
+++ FarmerApi/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,9 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_DIRS=[os.path.join(BASE_DIR,'store/static')]
 MEDIA_URL = '/media/' # URL prefix for media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
